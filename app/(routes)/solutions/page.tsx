5a56d328b2a2a118d353504a0b2bf1b42f5fabfb
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-03-31 10:11:25 +0600

    fix solution page responsive issues

--- app/(routes)/solutions/page.tsx
+++ app/(routes)/solutions/page.tsx
@@ -18,11 +18,11 @@ const Page: NextPage<Props> = ({}) => {
             <span className="h-[7px] w-[7px] bg-primary rounded-full inline-block"></span>
             <p>Our Tools</p>
           </div>
-          <h3 className="text-[42px] mt-[30px] mb-[90px]">
+          <h3 className="text-[30px] md:text-[35px] lg:text-[42px] mt-[30px] mb-[90px]">
             AI Development <span className="highlight">Tech Stack</span>
           </h3>
 
-          <div className="grid grid-cols-4 gap-[30px]">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[30px]">
             <TechnologyCard title="JavaScript" />
             <TechnologyCard title="Python" />
             <TechnologyCard title="GCloud" />
