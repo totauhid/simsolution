55e417b9a659049c621ac70325886d421b8d04fd
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-02 10:12:39 +0600

    fix home page responsive issues

--- app/components/ui/ServiceCard.tsx
+++ app/components/ui/ServiceCard.tsx
@@ -14,12 +14,12 @@ const ServiceCard: NextPage<Props> = ({ img, serial, title, description }) => {
   return (
     <div>
       <SingleDivider />
-      <div className="flex gap-20 py-[30px]">
+      <div className="flex gap-10 lg:gap-20 py-[30px]">
         <div className="relative help-circle h-[45px] w-[45px] shrink-0 ml-[30px] z-10 after:bg-primary">
           <Image src={img} fill alt="icon" />
         </div>
         <div>
-          <div className="flex items-center gap-4 text-[18px] mt-[10px] mb-[30px]">
+          <div className="flex items-center gap-4 text-[18px] lg:mt-[10px] mb-[30px]">
             <span className="highlight">{serial}</span>
             <h5>{title}</h5>
           </div>
