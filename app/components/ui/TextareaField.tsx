e68f038b4dbbfb528f42c28906c657ec6c5241ae
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-03 11:57:15 +0600

    added form validation

--- app/components/ui/TextareaField.tsx
+++ app/components/ui/TextareaField.tsx
@@ -3,14 +3,16 @@ import { NextPage } from "next";
 interface Props {
   label: string;
   placeholder: string;
+  register?: object;
 }
 
-const TextareaField: NextPage<Props> = ({ label, placeholder }) => {
+const TextareaField: NextPage<Props> = ({ label, placeholder, register }) => {
   return (
     <label className="shrink-0 flex-grow mb-[30px]">
       <span className="text-[16px] mb-[25px] inline-block">{label}</span>
 
       <textarea
+        {...register}
         rows={4}
         className="border-b-2 focus:border-[#12182066] w-full focus:outline-0"
         placeholder={placeholder}
