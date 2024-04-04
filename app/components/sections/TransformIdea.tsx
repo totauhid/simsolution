2b410c62120867734eff2216c7fca401c5e745dc
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-04-04 16:04:49 +0600

    remove text

--- app/components/sections/TransformIdea.tsx
+++ app/components/sections/TransformIdea.tsx
@@ -35,7 +35,7 @@ const TransformIdea = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
               ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas
-              accumsan lacus vel facilisis.s
+              accumsan lacus vel facilisis.
             </p>
 
             <div className="flex flex-col gap-[15px]">
