44531ad96a1b833f110148aad01dc49d2d86299c
md-asikuzzaman <mdasikuzzaman.en@example.com>
2024-04-24 18:53:28 +0600

    added modal with data loading

--- lib/store.ts
+++ lib/store.ts
@@ -1,11 +1,34 @@
 import { create } from "zustand";
 
-type StickyType = {
+type StickyState = {
   stickyActive: boolean;
   setStickyActive: (val: boolean) => void;
 };
 
-export const useStickyStore = create<StickyType>()((set) => ({
+export const useStickyStore = create<StickyState>()((set) => ({
   stickyActive: false,
   setStickyActive: (val) => set((prevState) => ({ stickyActive: val })),
+}));
+
+type ModalState = {
+  modal: boolean;
+  setModal: () => void;
+};
+
+export const useModalStore = create<ModalState>((set) => ({
+  modal: false,
+  setModal: () =>
+    set((prevState) => ({
+      modal: !prevState.modal,
+    })),
+}));
+
+type MessageIdState = {
+  id: string;
+  setId: (val: string) => void;
+};
+
+export const useIdStore = create<MessageIdState>()((set) => ({
+  id: "",
+  setId: (val) => set(() => ({ id: val })),
 }));
