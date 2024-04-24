e51a8318f4a47a3808bc9d6672c266c1baf8c729
Md-Asikuzzaman <mdasikuzzaman.en@gmail.com>
2024-04-24 13:06:43 +0600

    added form design

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h2>Dashboard page</h2>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/signin/page.tsx
@@ -0,0 +1,51 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Page: NextPage<Props> = ({}) => {
+  return (
+    <div className="h-screen bg-gray-300 flex items-center justify-center p-10">
+      <form className="max-w-[500px] w-full mx-auto bg-white p-8 rounded-lg">
+        <h2 className="text-3xl mb-4">Sign In</h2>
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your email
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="name@gmail.com"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="password"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Your password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="******"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Page;
